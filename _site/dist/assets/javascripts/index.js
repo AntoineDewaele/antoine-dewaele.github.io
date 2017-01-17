c2acdeb4ca3652e80bbb105697a768f7bf18f46b
Antoine Dewaele <antoinee.dewaele@gmail.com>
2017-01-17 18:10:30 +0000

    add portfolio + skills

---
+++ _site/dist/assets/javascripts/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 1500,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 4,
+                nav: false
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+    return paraxify('.paraxify');
+});
---
+++ _site/src/assets/javascripts/index.coffee
@@ -0,0 +1,24 @@
+$(document).ready(->
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:false,
+    responsive:{
+      0:{
+        items:2,
+      },
+      600:{
+        items:4,
+        nav:false
+      },
+      1000:{
+        items:6,
+      }
+    }
+  })
+
+  paraxify('.paraxify');
+);
